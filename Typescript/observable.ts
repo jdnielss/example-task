425e01f155d40c9e37d87a6023e368f1d451a4dd
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-03 20:06:14 +0700

    feat(typescript): add function observable

--- Typescript/observable.ts
+++ Typescript/observable.ts
@@ -9,3 +9,24 @@ const user = {
   name: "Andy",
   age: 20,
 };
+
+/**
+ * Observable merupakan pattern untuk memanggil fungsi ketika sebuah object berubah nilainya
+ * contoh dan penggunaannya: https://codesandbox.io/s/ts-proxy-observable-h1yln?file=/src/index.ts
+ * @param obj Object dasar yang menjadi pondasi
+ * @param onChange fungsi yang akan dipanggil ketika object dasar berubah nilai property nya
+ */
+function observable<T>(obj: T, onChange: ObservableChange<T>): T {
+  // callback akan dipanggil pertama kali ketika fungsi observable dipanggil
+  onChange(obj, obj);
+
+  return new Proxy(obj as Object, {
+    set: function (target: T, key, newValue) {
+      const oldValue = { ...target };
+      // mengubah nilai object berdasarkan property yang diubah nilainya
+      Reflect.set(target as Object, key, newValue);
+      onChange(oldValue, target);
+      return true;
+    },
+  }) as T;
+}
