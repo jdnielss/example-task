80ad76e06268fb92090e584fdc15a9bc694c875f
Hasbi_CL <hasbi22109@gmail.com>
2021-10-29 22:04:07 +0700

    Basic C++ get count difference number

---
+++ c++/math/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int countNumber();
+
+int main() {
+    float firstNumber = 123.456;
+    float secondNumber = 76.543;
+
+    cout << "Sum : " << firstNumber + secondNumber << endl;
+    cout << "Difference : " << firstNumber - secondNumber;
+    return 0;
+}
